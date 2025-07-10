dd774ee33f715ff9d81676251768c04acebdaca6
faharetana-rkt <rakotomaharitra@gmail.com>
2025-07-10 16:27:05 +0300

    finished event listener

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,13 +3,18 @@ const addButton = document.querySelector("button");
 const list = document.querySelector("#list");
 
 addButton.addEventListener("click", () => {
+  if (favchap.value.trim() !== "") {
     const scripture = document.createElement("li");
     const deleteButton = document.createElement("button");
     scripture.innerHTML = favchap.value;
     deleteButton.textContent = "❌";
     scripture.appendChild(deleteButton);
     list.appendChild(scripture);
-    deleteButton.addEventListener("click", () => list.remove());
-    favchap.textContent = "";
+    deleteButton.addEventListener("click", () => {
+      list.removeChild(scripture);
+      favchap.focus();
+    });
+    favchap.value = "";
     favchap.focus();
+  }
 });
